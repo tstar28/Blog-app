99e9fe0c48035605862d693b8776978d520fdf57
tstar28 <tushar.singh28apr@gmai.com>
2024-02-21 12:52:13 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const path = require("path");
 require("dotenv").config();
 const app = express();
 app.use("/images", express.static(path.join(__dirname, "/images")));
-
+app.use(cors());
 app.use(express.json());
 
 mongoose
